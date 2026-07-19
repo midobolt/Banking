ba2da3fb50d7fd74b84d4a325a33bf29b69d9051
midobolt <ahmad.senbol@yahoo.com>
2026-07-19 15:41:17 +0200

    Implement initial file and folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,26 @@
-import { Geist, Geist_Mono, IBM_Plex_Sans, Nunito_Sans } from "next/font/google"
+import { Geist_Mono, IBM_Plex_Sans, Nunito_Sans } from "next/font/google"
+import type { Metadata } from "next";
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const nunitoSansHeading = Nunito_Sans({subsets:['latin'],variable:'--font-heading'});
+const nunitoSansHeading = Nunito_Sans({subsets:['latin'],variable:'--font-heading', display: "swap",});
 
-const ibmPlexSans = IBM_Plex_Sans({subsets:['latin'],variable:'--font-sans'})
+const ibmPlexSans = IBM_Plex_Sans({subsets:['latin'],variable:'--font-ibm', display: "swap",})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone.",
+  icons: {
+    icon: '/icons/logo.svg' 
+  }
+};
 
 export default function RootLayout({
   children,
